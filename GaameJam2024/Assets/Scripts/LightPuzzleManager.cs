46c94a07bd7096b3a6fc737b127bff25ca658447
LeahTaurisano <leahtaurisano@gmail.com>
2024-01-22 23:14:23 -0800

    TestingLightPuzzle

---
+++ GaameJam2024/Assets/Scripts/LightPuzzleManager.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.IK;
+
+public class LightPuzzleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject lightBox;
+
+    private static int dimensions = 5;
+    private static Lights lightComponent;
+    private static List<List<GameObject>> lights;
+    private static bool solved = false;
+    private bool created = false;
+    private bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = new List<List<GameObject>>();
+    }
+
+    private void Update()
+    {
+        if (solved)
+        {
+            foreach (List<GameObject> row in lights)
+            {
+                foreach (GameObject light in row)
+                {
+                    light.GetComponent<SpriteRenderer>().color = Color.green;
+                }
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!created)
+            {
+                CreatePuzzle();
+            }
+            else if (open)
+            {
+                foreach (List<GameObject> row in lights)
+                {
+                    foreach (GameObject light in row)
+                    {
+                        light.GetComponent<SpriteRenderer>().enabled = false;
+                        light.GetComponent<BoxCollider2D>().enabled = false;
+                        open = false;
+                    }
+                }
+            }
+            else
+            {
+                foreach (List<GameObject> row in lights)
+                {
+                    foreach (GameObject light in row)
+                    {
+                        light.GetComponent<SpriteRenderer>().enabled = true;
+                        light.GetComponent<BoxCollider2D>().enabled = true;
+                        open = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void CreatePuzzle()
+    {
+        for (int y = 0; y < dimensions; ++y)
+        {
+            List<GameObject> row = new List<GameObject>();
+            for (int x = 0; x < dimensions; ++x)
+            {
+                GameObject toAdd = Instantiate(lightBox, new Vector3(x, y, 0), Quaternion.identity);
+                Lights lightComponent = toAdd.GetComponent<Lights>();
+                lightComponent.SetIndex(x, y);
+                row.Add(toAdd);
+            }
+            lights.Add(row);
+        }
+        created = true;
+        open = true;
+    }
+
+    public static bool GetSolved()
+    {
+        return solved;
+    }
+
+    public static void FlipLights(Lights light)
+    {
+        light.FlipActive();
+        int xIndex = light.GetIndex().x;
+        int yIndex = light.GetIndex().y;
+
+
+        if (xIndex - 1 >= 0)
+        {
+            lightComponent = lights[yIndex][xIndex - 1].GetComponent<Lights>();
+            lightComponent.FlipActive();
+        }
+        if (xIndex + 1 < dimensions)
+        {
+            lightComponent = lights[yIndex][xIndex + 1].GetComponent<Lights>();
+            lightComponent.FlipActive();
+        }
+        if (yIndex - 1 >= 0)
+        {
+            lightComponent = lights[yIndex - 1][xIndex].GetComponent<Lights>();
+            lightComponent.FlipActive();
+        }
+        if (yIndex + 1 < dimensions)
+        {
+            lightComponent = lights[yIndex + 1][xIndex].GetComponent<Lights>();
+            lightComponent.FlipActive();
+        }
+        solved = true;
+        foreach (List<GameObject> row in lights)
+        {
+            foreach (GameObject toCheck in row)
+            {
+                if (!toCheck.GetComponent<Lights>().GetActive())
+                {
+                    solved = false;
+                }
+            }
+        }
+
+    }
+}
---
+++ GaameJam2024/Assets/Scripts/Lights.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    private bool active;
+    private SpriteRenderer lightSprite;
+    private Lights lightComponent;
+    public int xPos, yPos;
+
+    private void Start()
+    {
+        lightSprite = gameObject.GetComponent<SpriteRenderer>();
+        lightComponent = gameObject.GetComponent<Lights>();
+        lightSprite.color = Color.gray;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0) && !LightPuzzleManager.GetSolved())
+        {
+            LightPuzzleManager.FlipLights(lightComponent);
+        }
+    }
+
+    public void FlipActive()
+    {
+        if (active)
+        {
+            active = false;
+            lightSprite.color = Color.gray;
+        }
+        else
+        {
+            active = true;
+            lightSprite.color = Color.yellow;
+        }
+    }
+
+    public bool GetActive()
+    {
+        return active;
+    }
+
+    public void SetIndex(int x, int y)
+    {
+        xPos = x;
+        yPos = y;
+    }
+    
+    public Vector2Int GetIndex()
+    {
+        return new Vector2Int(xPos, yPos);
+    }
+}
